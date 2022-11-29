43baf3b5221345b27b4de6fa67b878109cde00fe
Bojan Vreco <bojanvreco069@gmail.com>
2022-11-29 18:23:52 +0100

    Slider functionality finished

--- src/components/homepage/SliderPages.tsx
+++ src/components/homepage/SliderPages.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   faChevronLeft,
   faChevronRight,
@@ -23,6 +23,7 @@ const SliderPages: React.FC<{ promotions: ExtendedPromotionProps[] }> = ({
   const [currPage, setCurrPage] = useState(1);
   const [isAnimRunning, setIsAnimRunning] = useState(false);
   const [isDragActive, setIsDragActive] = useState(false);
+  const [visibilityChange, setVisibilityChange] = useState(false);
   const transitionRef = useRef('transform 0.35s linear');
   const dragStartRef = useRef(0);
   const dragPercentageRef = useRef(0);
@@ -55,82 +56,81 @@ const SliderPages: React.FC<{ promotions: ExtendedPromotionProps[] }> = ({
     }
   };
 
-  useEffect(() => {
-    transitionRef.current = isDragActive
-      ? 'transform 0 linear'
-      : 'transform 0.35s linear';
-    const timer = setInterval(() => {
-      if (currPage >= promotions.length - 1) return;
+  const handleDragStart = (e: React.DragEvent | React.TouchEvent) => {
+    if ('clientX' in e) {
+      dragStartRef.current = e.clientX;
+    }
 
-      if (!isDragActive) setCurrPage((prevState) => prevState + 1);
-    }, 5125125 * 1000);
+    if ('changedTouches' in e) {
+      dragStartRef.current = e?.changedTouches[0].clientX;
+    }
+
+    dragStartPageValueRef.current = currPage;
+
+    setIsDragActive(true);
+  };
+
+  const handleDragMove = (e: React.DragEvent | React.TouchEvent) => {
+    let dragPercentage = 0;
+
+    if ('clientX' in e)
+      dragPercentage = -(e.clientX - dragStartRef.current) / window.innerWidth;
+
+    if ('changedTouches' in e)
+      dragPercentage =
+        -(e.changedTouches[0].clientX - dragStartRef.current) /
+        window.innerWidth;
+
+    dragPercentageRef.current = dragPercentage;
+
+    const movePercentage = dragStartPageValueRef.current + dragPercentage;
+
+    setCurrPage(movePercentage);
+  };
+
+  const handleDragEnd = () => {
+    setIsDragActive(false);
+
+    transitionRef.current = 'transform 0.35s linear';
+
+    setTimeout(() => {
+      if (currPage < 0 || currPage >= promotions.length - 1) {
+        setCurrPage(dragStartPageValueRef.current);
+        return;
+      }
+
+      setCurrPage((prevState) => Math.round(prevState));
+    }, 50);
+  };
+
+  useEffect(() => {
+    if (visibilityChange) return;
+    transitionRef.current = isDragActive ? 'none' : 'transform 0.35s linear';
+
+    const timer = setInterval(() => {
+      setCurrPage((prevPage) => prevPage + 1);
+    }, 10 * 1000);
 
     return () => {
       clearInterval(timer);
     };
-  }, [currPage, promotions, isDragActive]);
+  }, [currPage, promotions, isDragActive, visibilityChange]);
+
+  useEffect(() => {
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'hidden') setVisibilityChange(true);
+      if (document.visibilityState === 'visible') setVisibilityChange(false);
+    });
+  }, []);
 
   return (
     <SliderMain
-      onDragStart={(e) => {
-        dragStartRef.current = e.clientX;
-
-        dragStartPageValueRef.current = currPage;
-
-        setIsDragActive(true);
-      }}
-      onDrag={(e) => {
-        const dragPercentage =
-          -(e.clientX - dragStartRef.current) / window.innerWidth;
-
-        dragPercentageRef.current = dragPercentage;
-
-        const movePercentage = dragStartPageValueRef.current + dragPercentage;
-
-        setCurrPage(Number(movePercentage.toFixed(2)));
-      }}
-      onDragEnd={(e) => {
-        setIsDragActive(false);
-
-        transitionRef.current = 'transform 0.35s linear';
-
-        if (currPage < 0 || currPage >= promotions.length - 1) {
-          setCurrPage(dragStartPageValueRef.current);
-          return;
-        }
-
-        setCurrPage((prevState) => Math.round(prevState));
-      }}
-      onTouchStart={(e) => {
-        dragStartRef.current = e.changedTouches[0].clientX;
-
-        dragStartPageValueRef.current = currPage;
-
-        setIsDragActive(true);
-      }}
-      onTouchMove={(e) => {
-        const dragPercentage =
-          -(e.changedTouches[0].clientX - dragStartRef.current) /
-          window.innerWidth;
-
-        dragPercentageRef.current = dragPercentage;
-
-        const movePercentage = dragStartPageValueRef.current + dragPercentage;
-
-        setCurrPage(movePercentage);
-      }}
-      onTouchEnd={() => {
-        setIsDragActive(false);
-
-        transitionRef.current = 'transform 0.35s linear';
-
-        if (currPage < 0 || currPage >= promotions.length - 1) {
-          setCurrPage(dragStartPageValueRef.current);
-          return;
-        }
-
-        setCurrPage((prevState) => Math.round(prevState));
-      }}
+      onDragStart={(e) => handleDragStart(e)}
+      onDrag={(e) => handleDragMove(e)}
+      onDragEnd={() => handleDragEnd()}
+      onTouchStart={(e) => handleDragStart(e)}
+      onTouchMove={(e) => handleDragMove(e)}
+      onTouchEnd={() => handleDragEnd()}
     >
       <SliderMainImgsWrapper
         imgsNum={promotions.length}
